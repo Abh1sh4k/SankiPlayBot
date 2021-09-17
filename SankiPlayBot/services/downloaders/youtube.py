ba197f08cab52afbfb28da75982cb218dba8c77a
Abh1sh4k <89832476+Abh1sh4k@users.noreply.github.com>
2021-09-17 10:27:12 +0530

    Update youtube.py

--- SankiPlayBot/services/downloaders/youtube.py
+++ SankiPlayBot/services/downloaders/youtube.py
@@ -21,12 +21,12 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Vιdεσ ιs Lσηgεr Thαη {DURATION_LIMIT} Μιηυτε's Arεη'τ Αllσωεd Tσ Ρlαy."
+            f"❌ Vιdεσ ιs Lσηgεr Thαη {DURATION_LIMIT} Μιηυτε's Arεη'τ Αllσωεd Tσ Ρlαy! For Any Query Pm @Abh1sh3k_Official ."
         )
     try:
         ydl.download([url])
     except:
         raise DurationLimitError(
-            f"❌ Vιdεσ ιs Lσηgεr Thαη {DURATION_LIMIT} Μιηυτε's Arεη'τ Αllσωεd Tσ Ρlαy."
+            f"❌ Vιdεσ ιs Lσηgεr Thαη {DURATION_LIMIT} Μιηυτε's Arεη'τ Αllσωεd Tσ Ρlαy! For Any Query Pm @Abh1sh3k_Official ."
         )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
