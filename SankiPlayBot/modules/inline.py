3dca5fb491b9bc4f010d90ea05d90cc9c785da3d
Abh1sh4k <89832476+Abh1sh4k@users.noreply.github.com>
2021-09-17 10:41:16 +0530

    Update inline.py

--- SankiPlayBot/modules/inline.py
+++ SankiPlayBot/modules/inline.py
@@ -16,7 +16,7 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Tyρε Αηy Υσυτυβε Vιdεσ Ναmε...",
+            switch_pm_text="Hey! I am Hydra PLay Bot Just Tyρε Αηy Υσυτυβε Vιdεσ Ναmε...",
             switch_pm_parameter="help",
             cache_time=0,
         )
